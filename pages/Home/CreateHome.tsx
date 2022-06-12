e62a36762b6f63642f1440ff0a02c0b3dc5cfb87
Debashish Dey <82308421+Debashish-Dey-DM@users.noreply.github.com>
2022-06-13 00:07:07 +0600

    may-fourth done

--- pages/Home/CreateHome.tsx
+++ pages/Home/CreateHome.tsx
@@ -1,37 +1,66 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import commonStyles from "../../styles/common.module.css";
 
 const CreateHome = () => {
-    const [newId,setNewId] = useState<string|undefined>();
-    const mount =async () => {
-        const res = await axios.get("http://localhost:3000/api/home/GetNewHomeId");
-        setNewId(res.data);
-    }
-    useEffect(() => {
-        mount();
-    }, []);
-    const submitData = async (e: any) => { 
-        e.preventDefault();
-        const rate = e.target?.[2]?.value;
-         const res = await axios.post("http://localhost:3000/api/home/createHome", { rate })
-         console.log(res.data);
-        
-    }
-    return (
-        <div>
-            <form onSubmit={submitData} className="pt-4">
-                <fieldset disabled>
-                    <label >ID</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder={newId}/>
-                </fieldset>
-                <div className="form-group">
-                    <label >Rate Per Month</label>
-                    <input type="text" className="form-control"  placeholder=""/>
-                </div>
-                <br />
-                <button type="submit" className="btn btn-primary ">Submit</button>
-            </form>
+  const [newId, setNewId] = useState<string | undefined>();
+  const mount = async () => {
+    const res = await axios.get("http://localhost:3000/api/home/GetNewHomeId");
+    setNewId(res.data);
+  };
+  useEffect(() => {
+    mount();
+  }, []);
+  const submitData = async (e: any) => {
+    e.preventDefault();
+    const rate = e.target?.[2]?.value;
+    const res = await axios.post("http://localhost:3000/api/home/createHome", {
+      rate,
+    });
+    console.log(res.data);
+  };
+  return (
+    <div className={` ${commonStyles.common} ${commonStyles.bgLightGrey} pt-5`}>
+          <Container className={`${commonStyles.commonForm} pt-3`}>
+              <h5>Ghor Toiri Korun</h5>
+        <Form className="py-4" onSubmit={submitData}>
+          <label className="ms-3">ID</label>
+          <Form.Control
+            type="number"
+            placeholder={newId}
+            name="name"
+            disabled
+          />
+          <br />
+          <label className="ms-3">Rate Per Month</label>
+          <Form.Control type="number" placeholder="(only number)" name="name" />
+          <br />
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+
+      {/* <form onSubmit={submitData} className="pt-4">
+        <fieldset disabled>
+          <label>ID</label>
+          <input
+            type="number"
+            className="form-control"
+            aria-describedby="emailHelp"
+            placeholder={newId}
+          />
+        </fieldset>
+        <div className="form-group">
+          <label>Rate Per Month</label>
+          <input type="number" className="form-control" placeholder="" />
         </div>
-    )
-}
+        <br />
+
+        <button type="submit" className="btn btn-primary ">
+          Submit
+        </button>
+      </form> */}
+    </div>
+  );
+};
 export default CreateHome;
--- pages/Payments/ShoshanDevDaho.tsx
+++ pages/Payments/ShoshanDevDaho.tsx
@@ -1,104 +1,143 @@
 import { Payment } from "@prisma/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-const ShoshanDevDaho = () => {
-    const [payment, setPayment] = useState<Payment[]>();
-    const [totalAmount,setTotalAmount] = useState(0);
-    const [pay, setPay] = useState({
-        type: "ShoshanDevDaho",
-        date: "",
-        amount: "",
-    })
-    const mount = async () => {
-         await axios.get("http://localhost:3000/api/Payments/getPayments/ShoshanDevDaho").
-            then(res => {
-                console.log(res.data);
-                setPayment(res.data);
-                let arr: number[] = [];
-                res.data?.map((item: Payment) => { arr.push(item.amount) })
-                const sum = arr.reduce((a, b) => a + b, 0);
-                setTotalAmount(sum);
-        })
-    }
-    useEffect(() => {mount()} , [])
-    const handleSubmit =async (e: any) => {
-        e.preventDefault();
+import styles from "./ShoshanDevDaho.module.css";
+import commonStyles from "../../styles/common.module.css";
+import { Button, Container } from "react-bootstrap";
 
-        const result = await axios.post("http://localhost:3000/api/Payments/createPayment", { pay }).
-            then(res => {
-                console.log(res.data);
-            }).catch(err => {
-                console.log(err);
-            })    
-        mount();
-    }
-    const handleChange = (e: any) => {
-        
-        const name = e.target.name;
-        const value = e.target.value;
-        if (name === "date") {
-            const newDate = new Date(value)
-            const updatedDate = newDate.toISOString();
-            setPay({ ...pay, [name]: updatedDate })
-        }
-        else {
-            setPay({ ...pay, [name]: value })
-        }
-    }
-    const test = () => {
+const ShoshanDevDaho = () => {
+  const [payment, setPayment] = useState<Payment[]>();
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [pay, setPay] = useState({
+    type: "ShoshanDevDaho",
+    date: "",
+    amount: "",
+  });
+  const mount = async () => {
+    await axios
+      .get("http://localhost:3000/api/Payments/getPayments/ShoshanDevDaho")
+      .then((res) => {
+        //   console.log(res.data[3]?.date.toLocaleDateString("en-US"));
+        //   var today = new Date(res.data[3]?.date);
+
+          console.log(new Date(res.data[3]?.date).toLocaleDateString("en-US"));
+        setPayment(res.data);
         let arr: number[] = [];
-        payment?.map((item: Payment) => { arr.push(item.amount) })
-        
+        res.data?.map((item: Payment) => {
+          arr.push(item.amount);
+        });
         const sum = arr.reduce((a, b) => a + b, 0);
-        console.log(sum);
-        
+        setTotalAmount(sum);
+      });
+  };
+  useEffect(() => {
+    mount();
+  }, []);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const result = await axios
+      .post("http://localhost:3000/api/Payments/createPayment", { pay })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    mount();
+  };
+  const handleChange = (e: any) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    if (name === "date") {
+      const newDate = new Date(value);
+      const updatedDate = newDate.toISOString();
+      setPay({ ...pay, [name]: updatedDate });
+    } else {
+      setPay({ ...pay, [name]: value });
     }
-    return (
-        <div className="container">
-            <div className="row align-items-start">
-                <div className="col">
-                    <h1>ShoshanDevDaho</h1>
-                <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="">amount</label>
-                    <input type="text" onChange={handleChange} name="amount" placeholder="amount" />
-                    <br />
-                    <label htmlFor="">Date</label>
-                    <input type="Date" onChange={handleChange} name="date" placeholder="Date" />
-                    <br />
-                    <button type="submit" >Submit</button>
+  };
+  const test = () => {
+    let arr: number[] = [];
+    payment?.map((item: Payment) => {
+      arr.push(item.amount);
+    });
+
+    const sum = arr.reduce((a, b) => a + b, 0);
+    console.log(sum);
+  };
+  return (
+    <div className={`${commonStyles.common} ${commonStyles.bgLightGrey}`}>
+      <h1>Shoshan Dev Daho</h1>
+
+      <Container
+        className={`${commonStyles.commonForm} ${styles.tblData} py-3`}
+      >
+        <div className="row">
+          <div className="col-lg-5 col-md-12">
+            <form onSubmit={handleSubmit} className="position-fixed ">
+              <div>
+                {/* myCode */}
+                <h6>পরিমাণ</h6>
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    placeholder="Amount"
+                    name="amount"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
                 </div>
+                <h6>তারিখ</h6>
+                <div className="input-group mb-3">
+                  <input
+                    type="Date"
+                    placeholder="Dates"
+                    name="date"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
+                </div>
+                {/* myCode */}
+
+                <Button type="submit">Submit</Button>
+              </div>
             </form>
-                </div>
-                <div className="col">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Amount</th>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                    {payment?.map((p,i) => {
-                        return (
-                            <tr key={i}>
-                                <td>{p.date}</td>
-                                <td>{p.amount}</td>
-                            </tr>
-                        )
-                    })}
-                    <tr>
-                                <td><h5>Total</h5></td>
-                                <td> {totalAmount}</td>
-                                
+          </div>
+          <div className="col-lg-7 col-md-12">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>তারিখ</th>
+                  <th>পরিমাণ</th>
+                </tr>
+              </thead>
+              <tbody>
+                {payment?.map((p, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{new Date(p.date).toLocaleDateString("bn-BD")}</td>
+                      <td>{p.amount}</td>
                     </tr>
-                        </tbody>
-                    </table>
-                    <button onClick={test}>test</button>
-                </div>
-            </div>
+                  );
+                })}
+                <tr>
+                  <td>
+                    <h6>
+                      <strong>মোট</strong>
+                    </h6>
+                  </td>
+                  <td>
+                    {" "}
+                    <strong>{totalAmount}</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
-}
+      </Container>
+    </div>
+  );
+};
 export default ShoshanDevDaho;
